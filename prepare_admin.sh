af8c78113f652ab73552e6307d58ae777b9280aa
Alex P <alexfile123@gmail.com>
2017-10-10 15:25:34 +0300

    update

--- prepare_admin.sh
+++ prepare_admin.sh
@@ -27,7 +27,7 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update
 apt-get install docker-ce -y 
 apt-get autoremove -y && apt-get autoclean && apt-get clean
-curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` \
+curl -sL https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` \
 	-o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose 
 
